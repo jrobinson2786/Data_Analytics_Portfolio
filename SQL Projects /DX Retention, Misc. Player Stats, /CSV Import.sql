d6d009d0a1bc83aae719d51939bc8bb37c8cd7da
Jack Robinson <118781481+jrobinson2786@users.noreply.github.com>
2023-09-13 22:19:08 -0600

    Update CSV Import.sql
    Fixing the path.. Copied file over from different directory.

--- SQL Projects /DX Retention, Misc. Player Stats, /CSV Import.sql
+++ SQL Projects /DX Retention, Misc. Player Stats, /CSV Import.sql
@@ -1,1 +1,31 @@
+-- Creating the database -- 
 
+CREATE TABLE first_login (
+	user_id varchar (10),
+	first_log_time timestamp
+);
+
+COPY first_login 
+FROM 'C:\Users\jackj\Desktop\Data Sets\VG data\player_last_login.csv'
+DELIMITER ',' CSV HEADER;
+
+
+CREATE TABLE last_login (
+	user_id varchar(10),
+	last_log_time timestamp
+);
+
+COPY last_login 
+FROM 'C:\Users\jackj\Desktop\Data Sets\VG data\player_last_logout.csv'
+DELIMITER ',' CSV HEADER;
+
+
+CREATE TABLE player_deaths (
+	user_id varchar(10),
+	time_death timestamp,
+	damage_taken integer
+);
+
+COPY player_deaths 
+FROM 'C:\Users\jackj\Desktop\Data Sets\VG data\player_deaths.csv'
+DELIMITER ',' CSV HEADER;
